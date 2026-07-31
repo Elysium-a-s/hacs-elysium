1ac310ad401f705bd18a5e7eadf63ae2ff59bc77
MrrTools <33814065+MrrTools@users.noreply.github.com>
2026-07-31 09:37:31 +0200

    Add native Elysium helper platforms

---
+++ custom_components/elysium/select.py
@@ -0,0 +1,16 @@
+from homeassistant.components.select import SelectEntity
+from .helper_base import ElysiumHelperMixin, setup_platform
+
+class ElysiumSelect(ElysiumHelperMixin, SelectEntity):
+    def __init__(self,hass,record,store):
+        self.initialize(hass,record,store);self.write_record_state()
+    def write_record_state(self):
+        self._attr_options=list(self._record.get("config",{}).get("options",["Option 1"]))
+        self._attr_current_option=str(self._record.get("state") or self._attr_options[0])
+        self._attr_icon="mdi:form-dropdown"
+    async def async_select_option(self,option):
+        if option not in self._attr_options: raise ValueError("Unknown option")
+        self._record["state"]=option;self.write_record_state();await self.persist();self.async_write_ha_state()
+
+async def async_setup_entry(hass,entry,async_add_entities):
+    setup_platform(hass,async_add_entities,{"select"},ElysiumSelect)
