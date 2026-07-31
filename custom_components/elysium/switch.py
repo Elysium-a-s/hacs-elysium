226bc6915d45ae6e6c639ab76e8de52ddb8cf1b9
MrrTools <33814065+MrrTools@users.noreply.github.com>
2026-07-31 09:37:04 +0200

    Use shared persistent helper infrastructure

--- custom_components/elysium/switch.py
+++ custom_components/elysium/switch.py
@@ -1,55 +1,27 @@
 from homeassistant.components.switch import SwitchEntity
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.util import slugify
+from .helper_base import ElysiumHelperMixin, setup_platform
 
-from .const import DOMAIN
-
-
-class ElysiumToggle(SwitchEntity):
-    _attr_should_poll = False
+class ElysiumToggle(ElysiumHelperMixin, SwitchEntity):
     _attr_icon = "mdi:toggle-switch"
 
     def __init__(self, hass, record, store):
-        self.hass = hass
-        self._record = record
-        self._store = store
-        self._attr_unique_id = f"elysium_helper_{record['helper_id']}"
-        self._attr_name = record["name"]
-        self._attr_is_on = bool(record.get("is_on", False))
-        self.entity_id = record.get("entity_id") or f"switch.elysium_{slugify(record['name'])}"
+        self.initialize(hass, record, store)
+        self.write_record_state()
+
+    def write_record_state(self):
+        self._attr_is_on = bool(self._record.get("state", False))
 
     async def async_turn_on(self, **kwargs):
-        await self._set_state(True)
-
-    async def async_turn_off(self, **kwargs):
-        await self._set_state(False)
-
-    async def _set_state(self, value):
-        self._attr_is_on = value
-        self._record["is_on"] = value
-        await self._store.async_save(self.hass.data[DOMAIN]["helpers"])
+        self._record["state"] = True
+        self.write_record_state()
+        await self.persist()
         self.async_write_ha_state()
 
+    async def async_turn_off(self, **kwargs):
+        self._record["state"] = False
+        self.write_record_state()
+        await self.persist()
+        self.async_write_ha_state()
 
-async def async_setup_entry(hass, entry, async_add_entities: AddEntitiesCallback):
-    data = hass.data[DOMAIN]
-    entities = []
-
-    def add_helper(record):
-        helper_id = str(record["helper_id"])
-        existing = data["entities"].get(helper_id)
-        if existing is not None:
-            return existing
-        entity = ElysiumToggle(hass, record, data["helper_store"])
-        data["entities"][helper_id] = entity
-        async_add_entities([entity])
-        return entity
-
-    data["add_toggle_helper"] = add_helper
-
-    for record in data["helpers"].values():
-        entities.append(ElysiumToggle(hass, record, data["helper_store"]))
-    for entity in entities:
-        data["entities"][str(entity._record["helper_id"])] = entity
-    if entities:
-        async_add_entities(entities)
+async def async_setup_entry(hass, entry, async_add_entities):
+    setup_platform(hass, async_add_entities, {"toggle"}, ElysiumToggle)
